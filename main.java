c174e4a2ee79a0bb435ed842d425b3ec5a83fe3f
publicStaticVoidMainUser <aryan.kothari@infineon.com>
2025-07-14 08:56:36 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,5 @@
-import java.util.*;
-public class jenkins-test{
-  public static void main(String args[]){
-    System.out.println("Testing");
-  }
+public class JenkinsTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+    }
 }
